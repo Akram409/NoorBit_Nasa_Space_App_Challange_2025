5834dfc743c6765c0ec4a2ed170d427c2cb6ab5b
Jobaieer <jobaieer.cse@proton.me>
2025-10-02 19:33:18 +0600

    feat(blog): integrate assets-blog data, update BlogPage UI

---
+++ src/pages/Dashboard/Blog/assets-blog.js
@@ -0,0 +1,53 @@
+import B1 from "../../../assets/BlogAssets/B1.webp";
+import B2 from "../../../assets/BlogAssets/B2.webp";
+import B3 from "../../../assets/BlogAssets/B3.webp";
+import B4 from "../../../assets/BlogAssets/B4.webp";
+import B5 from "../../../assets/BlogAssets/B5.webp";
+
+export const BlogImg = [
+  {
+    id: 1,
+    title: "Pollution Global",
+    topic: "Environment",
+    des: "Humanity achieved a fateful milestone last year. The European Union's Copernicus Climate Change Service officially declared 2024 the hottest year on record.",
+    image: B1,
+  },
+  { id: 2, title: "Heat and Pollution Threaten Human Well-being", image: B2 },
+  {
+    id: 3,
+    title: "Pollution by wind blown lead mine waste",
+    topic: "Environment",
+    image: B3,
+  },
+  {
+    id: 4,
+    title: "Traffic & Wind Effects on Green Infrastructure for Air Quality",
+    topic: "Air Quality",
+    image: B4,
+  },
+  { id: 5, title: "Critical Weather", topic: "Weather", image: B5 },
+];
+
+export const BlogVideo = [
+  {
+    id: 1,
+    title: "How NASA Sees the Air We Breathe",
+    url: "https://www.youtube.com/embed/Rizlv2n3NoQ?si=YdbEdy3fknnncrBO",
+  },
+  {
+    id: 2,
+    title: "TEMPO: Exploring Air Quality",
+    url: "https://www.youtube.com/embed/drh5k2uE-lk?si=oCjb3A1GkfAxdsam",
+  },
+  {
+    id: 3,
+    title:
+      "NASA Science Live: Climate Edition - Innovations for a Sustainable Future",
+    url: "https://www.youtube.com/embed/feF41iREr90?si=AlA7rsmQor8Ai6G7",
+  },
+  {
+    id: 4,
+    title: "NASA maps air pollution over cities in 3D",
+    url: "https://www.youtube.com/embed/Rk3svhGc2gA?si=ZDDVewOhifZKCNdr",
+  },
+];
